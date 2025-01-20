047db7182bf7e770fe92c89cdd6fc79f37dd0537
ritushiragannavar21 <120014472+ritushiragannavar21@users.noreply.github.com>
2025-01-20 16:05:08 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./sections/*.{html,js,liquid", 
-      "./snippets/*.{html,js,liquid",
+      "./sections/*.{html,js,liquid}", 
+      "./snippets/*.{html,js,liquid}",
       "./js/**/*.{js,svelte,jsx}",  ],
     theme: {
       extend: {},
